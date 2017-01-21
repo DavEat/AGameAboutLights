b1fb11361cc0af03b193857e6d072c2a51b93a38
Valentin Lofberg <lofbergvalentin@gmail.com>
2017-01-21 16:25:57 +0100

    Finishing the menu

---
+++ AGameAboutLights/Assets/Scripts/AG_SelectLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AG_SelectLevel : MonoBehaviour {
+
+    public void loadLevel()
+    {
+       int level = int.Parse(this.GetComponentInChildren<UnityEngine.UI.Text>().text);
+       SceneManager.LoadScene("Level" + level);
+    } 
+}
