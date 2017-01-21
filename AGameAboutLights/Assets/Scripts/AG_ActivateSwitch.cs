4121df5edf8c52f97537df5303997e05391041a6
Valentin Lofberg <lofbergvalentin@gmail.com>
2017-01-21 18:21:20 +0100

    Adding the aniation for the switch

---
+++ AGameAboutLights/Assets/Scripts/AG_ActivateSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AG_ActivateSwitch : MonoBehaviour {
+
+    private bool activated;
+
+    private void Start()
+    {
+        activated = false;
+    }
+
+    public void activate()
+    {
+        if (activated == false)
+        {
+            GetComponent<Animator>().SetFloat("speed", 1.5f);
+            GetComponent<Animator>().Play("SwitchAnimation");
+        }
+        else
+        {
+            GetComponent<Animator>().SetFloat("speed", -1.5f);
+            GetComponent<Animator>().Play("SwitchAnimation");
+        }
+        activated = !activated;
+    }
+}
