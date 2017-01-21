ac1eb025df7d1cd4208ffef22bb2c9e0bf017b77
Valentin Lofberg <lofbergvalentin@gmail.com>
2017-01-21 11:52:48 +0100

    Adding the menu

---
+++ AGameAboutLights/Assets/Scripts/AG_Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AG_Menu : MonoBehaviour {
+
+    public GameObject levelsPanel, creditsPanel, flashPanel;
+
+    public void displayLevels()
+    {
+        levelsPanel.SetActive(!levelsPanel.active);
+    }
+
+    public void displayCredits()
+    {
+        creditsPanel.SetActive(!creditsPanel.active);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
