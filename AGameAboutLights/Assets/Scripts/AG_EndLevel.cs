ee3ca58c29d25b370a15c530569f764bbc1e554a
Valentin Lofberg <lofbergvalentin@gmail.com>
2017-01-22 01:16:11 +0100

    Creating the end of level box

---
+++ AGameAboutLights/Assets/Scripts/AG_EndLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AG_EndLevel : MonoBehaviour {
+
+    public void nextLevel()
+    {
+        string level = SceneManager.GetActiveScene().name;
+        int levelToLoad = int.Parse(level.Remove(0, 5)) + 1;
+        SceneManager.LoadScene("Level" + levelToLoad);
+    }
+
+    public void backToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
