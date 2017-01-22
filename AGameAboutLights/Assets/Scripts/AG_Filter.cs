aa7c7bce5f40c9579a54024fbd6eb4c9b60c9f5d
DavEat <oned@hotmail.be>
2017-01-22 11:43:59 +0100

    Filter and receiver with color

--- AGameAboutLights/Assets/Scripts/AG_Filter.cs
+++ AGameAboutLights/Assets/Scripts/AG_Filter.cs
@@ -1,4 +1,15 @@
-﻿public class AG_Filter : AG_ElementType
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AG_Filter : AG_ElementType
 {
-    public int colorIndex;     
+    public int colorIndex;
+    public Image filterColorImage;
+
+    void Start()
+    {
+        if (colorIndex != -1)
+            filterColorImage.color = AG_Color.colorList[colorIndex];
+        else filterColorImage.color = Color.white;
+    }
 }
---
+++ AGameAboutLights/Assets/Scripts/AG_Receiver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AG_Receiver : AG_ElementType 
+{
+    public int colorIndex;
+    public Image receiverColorImage;
+
+    void Start()
+    {
+        if (colorIndex != -1)
+            receiverColorImage.color = AG_Color.colorList[colorIndex];
+        else receiverColorImage.color = Color.white;
+    }
+}
